b62e77c1d72451dcba07d0f7fb733113b73487f6
lucas-leme <lucaslssantos99@gmail.com>
2020-06-20 21:10:03 -0300

    Updates

---
+++ precache-manifest.33132a60ea19fbd47e911268b2617301.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c18b0938cd4bf14c304c1617894917a",
+    "url": "/lucas-leme-site/index.html"
+  },
+  {
+    "revision": "2a0d74c655aa846e62d1",
+    "url": "/lucas-leme-site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2be4c1f0ca55e9b621e8",
+    "url": "/lucas-leme-site/static/js/2.f66b790a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lucas-leme-site/static/js/2.f66b790a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a0d74c655aa846e62d1",
+    "url": "/lucas-leme-site/static/js/main.7a043ef9.chunk.js"
+  },
+  {
+    "revision": "4b7a9477643143b0c9d9",
+    "url": "/lucas-leme-site/static/js/runtime-main.87c3c93a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lucas-leme-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
